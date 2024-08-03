01ed0e4da8ffaa0cc9ff9777777fed47c75e7f66
unknown <aliyubarde97@gmail.com>
2024-08-03 12:45:16 +0100

    applying validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import { FieldValues, useForm } from 'react-hook-form';
 
+
+interface FormData {
+  name:String,
+  age:Number
+}
+
 function App() {
 
-  const {register, handleSubmit} = useForm( );
- 
-
-  const onSubmit = (data:FieldValues) => console.log(data)
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const onSubmit = (data: FieldValues) => console.log(data)
 
   return (
     <>
@@ -14,12 +18,14 @@ function App() {
         <form action="" onSubmit={handleSubmit(onSubmit)}>
           <div>
             <label htmlFor="">Name</label><br />
-            <input type="text" id='name' {...register('name')} />
+            <input type="text" id='name' {...register('name', { required: true, minLength: 3 })}  />
+            { errors.name?.type === 'required' && <p>this filled is required</p>}
+            { errors.name?.type === 'minLength' && <p>must be atlist morether 3 character</p>}
           </div>
 
           <div>
             <label htmlFor="">age</label><br />
-            <input type="number"  {...register('age')}  id="age" />
+            <input type="number"  {...register('age')} id="age" />
           </div>
 
           <div>
